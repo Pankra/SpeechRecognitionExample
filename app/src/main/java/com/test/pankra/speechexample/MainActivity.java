da9a7dac0c3202a636ca78bc933dcbad9f1a2d02
pankratov <pankratov@vigroup.ru>
2017-10-25 11:14:13 +0500

    speech recognition w/o dialog

--- app/src/main/java/com/test/pankra/speechexample/MainActivity.java
+++ app/src/main/java/com/test/pankra/speechexample/MainActivity.java
@@ -2,9 +2,12 @@ package com.test.pankra.speechexample;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.speech.RecognizerIntent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,12 +15,74 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     private static final int REQUEST_CODE_VOICE = 1;
+    private SpeechRecognizer speechRecognizer;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.text);
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        //https://stackoverflow.com/questions/6316937/how-can-i-use-speech-recognition-without-the-annoying-dialog-in-android-phones --look for the answer in the question
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+                Log.d(TAG, "onReadyForSpeech");
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                Log.d(TAG, "onBeginningOfSpeech");
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+                Log.d(TAG, "onRmsChanged");
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+                Log.d(TAG, "onBufferReceived");
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+                Log.d(TAG, "onEndofSpeech");
+            }
+
+            @Override
+            public void onError(int error) {
+                Log.d(TAG,  "error " +  error);
+                textView.setText("error " +  error);
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+                String str = "\n";
+                Log.d(TAG, "onResults " + results);
+                ArrayList data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                for (int i = 0; i < data.size(); i++)
+                {
+                    Log.d(TAG, "result " + data.get(i));
+                    str += "\n" + data.get(i);
+                }
+                textView.setText("results: " + String.valueOf(data.size()) + str);
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+                Log.d(TAG, "onPartialResults");
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle bundle) {
+                Log.d(TAG, "onEvent " + eventType);
+            }
+        });
+
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,6 +93,23 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(Intent.createChooser(intent, null), REQUEST_CODE_VOICE);
             }
         });
+
+        findViewById(R.id.button_no_dialog).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ru-RU");
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.label_speak_now));
+
+                speechRecognizer.startListening(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        speechRecognizer.destroy();
     }
 
     @Override
@@ -39,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
                     ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     if (matches != null && !matches.isEmpty()) {
                         String result = matches.get(0);
-                        ((TextView) findViewById(R.id.text)).setText(result);
+                        textView.setText(result);
                     }
                     break;
             }
